682ae0447885da3817d4fc790f20f532fd128634
xbisatrouble <xbisatrouble@gmail.com>
2022-04-04 21:31:04 +0800

    test

---
+++ src/main/java/com/team3/phms/service/DietService.java
@@ -0,0 +1,42 @@
+package com.team3.phms.service;
+
+import com.team3.phms.models.Diet;
+import com.team3.phms.models.User;
+import com.team3.phms.repository.DietRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class DietService {
+    final DietRepository dietRepository;
+
+    public DietService(DietRepository dietRepository) {
+        this.dietRepository = dietRepository;
+    }
+
+    public List<Diet> GetAll() {
+        return this.dietRepository.findAll();
+    }
+
+    public Optional<Diet> Get(Long id) {
+        return this.dietRepository.findById(id);
+    }
+
+    public Diet Create(String height, String weight, String plan, User user) {
+        Diet diet = new Diet(height, weight, plan);
+        double h = Double.parseDouble(height);
+        double w = Double.parseDouble(weight);
+        double b = w/h/h;
+        String bmi = String.format("%.2f", b);
+        diet.setBmi(bmi);
+        diet.setUser(user);
+        return dietRepository.save(diet);
+    }
+
+    public boolean Delete(Diet diet) {
+        dietRepository.delete(diet);
+        return true;
+    }
+}
